f741c287e5c322ff232f07d9ae1ac33036482ff6
Anirudhya Sarker <anirudhya.sarker@bracits.com>
2026-09-07 12:41:09 +0600

    Initial scaffolding.

---
+++ src/main/java/com/rootcause/fosholdoctor/FosholDoctorApplication.java
@@ -0,0 +1,13 @@
+package com.rootcause.fosholdoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FosholDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FosholDoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rootcause/fosholdoctor/FosholDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootcause.fosholdoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FosholDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
